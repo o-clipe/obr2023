c96b8f1e38e9260da2e7e5a122a454d370ca9888
o-clipe <luksnapo@gmail.com>
2023-06-16 18:04:38 -0300

    Adiciona biblioteca Linha

---
+++ lib/Linha/Linha.cpp
@@ -0,0 +1,22 @@
+/*Algoritimo para o robo seguir a linha - Codigo*/
+
+#include "Arduino.h"
+#include "Lum.h"
+#include "Linha.h"
+
+Linha::Linha() // Constructor
+{
+
+}
+
+void Linha::setup() // Chamado no Setup()
+{
+
+}
+
+void Linha::run() // Comandos de rotina
+{
+  
+}
+
+
---
+++ lib/Linha/Linha.h
@@ -0,0 +1,18 @@
+/*Algoritimo para o robo seguir a linha*/
+
+#ifndef Linha_h
+#define Linha_h
+
+#include "Arduino.h"
+#include "Lum.h"
+
+class Linha{
+  public:
+    Linha();
+    void setup();
+    void run();
+  private:
+    int _none;
+};
+
+#endif
