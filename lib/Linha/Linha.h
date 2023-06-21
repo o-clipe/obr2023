bad9ed545defc085f0be3bd0d6f820cc07815e8a
O-CLIPE <luksnapo@gmail.com>
2023-06-21 20:27:50 -0300

    Em teoria só existe uma única rotina possível.

--- lib/Linha/Linha.h
+++ lib/Linha/Linha.h
@@ -16,40 +16,12 @@ class Linha
     Linha(Lum& lum_obj, Motor& motor_obj, Giros& giros_obj);
     void setup();
     void run();
-    uint8_t checkState(uint16_t sensorsPosition[5]);
-    void pararSeOutroStatus(uint8_t f_status);
     bool segueLinha();
-    bool girar90Graus(char l=' ');
-    bool paralelar();
-    bool smoothCurve();
-    bool trialAndErrorCurve();
-    bool gap();
-    bool micro();
 
-    void definePerfeitamenteNaLinha();
-    bool _paralelarLadoE();
-    bool _paralelarLadoD();
-    bool _simples();
-
-    uint8_t status;
   private:
     Lum& _lum;
     Motor& _carro;
     Giros& _giros;
-    bool _boolLoop=true;  // _boolLoop é sempre, quando não está sendo usado, _true_. Ou seja, deve sempre existir antes de return false;
-    float _startGirosPos[3];
-    char _paralelarLado;
-    bool _paralelarLadoDone=false;
-    uint8_t _paralelarBest=0;
-    bool _paralelarBestAssigned=false;
-    bool _paralelarLoop=true;
-    unsigned long _trialAndErrorTime=0;
-    uint16_t _perfeitamenteNaLinha[5];
-    uint8_t _rotina = 0;
-    void* _rotinas[10];
-    bool _getRotina(uint8_t id);
-    char _girar90GrausLado = 'l';
-    uint8_t _trialAndErrorStage;
 };
 
 #endif
