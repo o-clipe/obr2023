edfd2f2e2bcff61d9ec1dea2e7b21a770dd1b3db
O-CLIPE <luksnapo@gmail.com>
2023-06-20 22:55:02 -0300

    Adiciona funções.
    Teoricamente se alinhar a linha é muito difícil.

--- lib/Linha/Linha.cpp
+++ lib/Linha/Linha.cpp
@@ -4,11 +4,27 @@
 #include "Linha.h"
 #include "Lum.h"
 #include "Motor.h"
+#include "Giros.h"
+
+#define Padrao 0
+#define None 1
+#define Inicio 2
+#define LinhaaEsquerda 10
+#define NoventaEsquerda 11
+#define LinhaaDireita 20
+#define NoventaDireita 21
+#define SemLinha 30
+#define SemiParaleloaLinha 40
+#define ParaleloaLinha 41
 
 
-Linha::Linha(Lum& lum_obj, Motor& motor_obj): _lum(lum_obj), _carro(motor_obj) // Constructor
+Linha::Linha(Lum& lum_obj, Motor& motor_obj, Giros& giros_obj): _lum(lum_obj), _carro(motor_obj), _giros(giros_obj) // Constructor
 {
-    status = "inicio";
+    status = Inicio;
+    _loopBool = true;
+    _paralelarBest = 0;
+    _paralelarLadoDone = false;
+    _paralelarBestAssigned = false;
 }
 
 
@@ -20,11 +36,11 @@ void Linha::setup() // Chamado no Setup()
 
 void Linha::run() // Comandos de rotina
 {
-  rotinaBasica();
+  segueLinha();
 }
 
 
-String Linha::checkState(uint16_t sensorsPosition[5])
+uint8_t Linha::checkState(uint16_t sensorsPosition[5])
 {
     uint16_t ee = _lum.normalizeSensEntry(0, sensorsPosition[0]);
     uint16_t e = _lum.normalizeSensEntry(1, sensorsPosition[1]);
@@ -34,17 +50,18 @@ String Linha::checkState(uint16_t sensorsPosition[5])
 
     Serial.print((String)ee + " " + (String)e + " " + (String)m + " " + (String)d + " " + (String)dd + ": ");
 
-    if (ee < e && e < m && m > d && d > dd) return "default";
-    else if (e >= m && m > d && d >= dd) return "linha a esquerda";
-    else if (ee <= e && e < m && m <= d) return "linha a direita";
-    else if (ee == 0 && e == 0 && m == 0 && d == 0 && dd == 0) return "sem linha";
-    else if (ee > 0 && e > 0 && m > 0 && d > 0 && dd > 0) return "paralelo a linha";
+    if (ee < e && e < m && m > d && d > dd) return Padrao;
+    if (e >= m && m > d && d >= dd) return LinhaaEsquerda;
+    if (ee <= e && e < m && m <= d) return LinhaaDireita;
+    if (ee == 0 && e == 0 && m == 0 && d == 0 && dd == 0) return SemLinha;
+    if (ee > OUTPUTRANGE/10 && e > OUTPUTRANGE/10 && m > OUTPUTRANGE/10 && d > OUTPUTRANGE/10 && dd > OUTPUTRANGE/10) return ParaleloaLinha;
+    if (ee > 0 && e > 0 && m > 0 && d > 0 && dd > 0) return SemiParaleloaLinha;
 
-    return "F";
+    return None;
 }
 
 
-void Linha::pararSeOutroStatus(String f_status)
+void Linha::pararSeOutroStatus(uint8_t f_status)
 {
     if (f_status != status)
     {
@@ -53,34 +70,177 @@ void Linha::pararSeOutroStatus(String f_status)
 }
 
 
-void Linha::rotinaBasica()
+bool Linha::segueLinha()
 {
-    String status = checkState(_lum.processedReadAll());
-    if (status == "default")
+    uint8_t status = checkState(_lum.processedLastMem());
+    if (status == Padrao)
     {
         _carro.ligarReto();
     }
-    if (status == "linha a esquerda")
+    if (status == LinhaaEsquerda)
     {
-        pararSeOutroStatus("linha a esquerda");
+        pararSeOutroStatus(LinhaaEsquerda);
         _carro.ligarMotor("e", VELOCIDADEDECURVA);
     }
-    if (status == "linha a direita")
+    if (status == LinhaaDireita)
     {
-        pararSeOutroStatus("linha a direita");
+        pararSeOutroStatus(LinhaaDireita);
         _carro.ligarMotor("d", VELOCIDADEDECURVA);
     }
-    if (status == "sem linha"){
-        _carro.parar();
-    }
-    else if (status == "paralelo a linha")
+    else
     {
-        _carro.ligarRe();
+        return false;
     }
-
+    return true;
 }
 
 
+bool Linha::girar90Graus(char l)
+{
+    char op;
+    if (l == 'e'){op='l';}else{op='e';}
+
+    if (_loopBool)
+    {
+    float* xyzRef = _giros.girosRead();
+    _startGirosPos[0] = xyzRef[0];
+    _startGirosPos[1] = xyzRef[1];
+    _startGirosPos[2] = xyzRef[2];
+    _carro.ligarMotor((String)l+"r", VELOCIDADEDECURVA/2);
+    _carro.ligarMotor((String)op, VELOCIDADEDECURVA/2);
+    _loopBool = false;
+    }
+    else
+    {
+        float* xyzRef = _giros.girosRead();
+        if (fmod((xyzRef[2]-90), 180) < _giros.variacaoPadrao[2]*1.5) // Possivel BUG, caso o robo gire muito rapido
+        {
+            _carro.parar();
+            _loopBool = true;
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool Linha::paralelar()  // necessario espaco, bastante
+{
+    uint8_t status = checkState(_lum.processedLastMem());
+    if (_loopBool)
+    {
+        _paralelarLadoDone = false;
+        if (status == LinhaaEsquerda)
+        {
+            _paralelarLado = 'e';
+        }
+        if (status == LinhaaDireita)
+        {
+            _paralelarLado = 'd';
+        }
+        _loopBool = false;
+        return true;
+    }
+    // if (status == ParaleloaLinha)
+    // {
+    //     _loopBool = true;
+    //     return false;
+    // }
+    if (_paralelarLado == 'e')
+    {
+        if(!_paralelarLadoDone)
+        {
+            if(_paralelarLadoE())
+            {
+                return true;
+            }
+            _paralelarLadoDone = true;
+        }
+        if (_paralelarLadoD()){return true;}else{_loopBool = true; return false;}
+    }
+    if (_paralelarLado == 'd')
+    {
+        if(!_paralelarLadoDone)
+        {
+            if(_paralelarLadoD())
+            {
+                return true;
+            }
+            _paralelarLadoDone = true;
+        }
+        if (_paralelarLadoE()){return true;}else{_loopBool = true; return false;} 
+    }
+}
+
+
+bool Linha::_paralelarLadoE()
+{
+    if(_lum.processedlastMemOutput[0] > _paralelarBest)
+    {
+        _paralelarBest = _lum.processedlastMemOutput[0];
+        _carro.ligarMotor("e", VELOCIDADEDECURVA/2);
+        return true;
+    }
+    if(_lum.processedlastMemOutput[0] < _paralelarBest)
+    {
+        _carro.ligarMotor("er", VELOCIDADEDECURVA/2);
+        _paralelarBestAssigned = true;
+        return true;
+    }
+    if(_lum.processedlastMemOutput[0] == _paralelarBest && _paralelarBestAssigned)
+    {
+        _carro.parar();
+        _paralelarBestAssigned = false;
+        _paralelarBest = 0;
+        return false;
+    }
+}
+
+
+bool Linha::_paralelarLadoD()
+{
+    if(_lum.processedlastMemOutput[4] < _paralelarBest)
+    {
+        _paralelarBest = _lum.processedlastMemOutput[0];
+        _carro.ligarMotor("d", VELOCIDADEDECURVA/2);
+        return true;
+    }
+    if(_lum.processedlastMemOutput[4] > _paralelarBest)
+    {
+        _carro.ligarMotor("dr", VELOCIDADEDECURVA/2);
+        _paralelarBestAssigned = true;
+        return true;
+    }
+    if(_lum.processedlastMemOutput[4] == _paralelarBest && _paralelarBestAssigned)
+    {
+        _carro.parar();
+        _paralelarBestAssigned = false;
+        _paralelarBest = 0;
+        return false;
+    }
+}
+
+
+bool Linha::alignInPlace()
+{
+    uint8_t status = checkState(_lum.processedLastMem());
+    if (status != Padrao)
+    {
+        if (_loopBool)
+        {
+            
+        }
+    }
+}
+
+
+bool Linha::gap()
+{
+    uint8_t status = checkState(_lum.processedLastMem());
+    if (status == SemLinha){
+        return true;
+    }
+}
 
 
  
--- lib/Linha/Linha.h
+++ lib/Linha/Linha.h
@@ -12,16 +12,31 @@
 class Linha
 {
   public:
-    Linha(Lum& lum_obj, Motor& motor_obj);
+    Linha(Lum& lum_obj, Motor& motor_obj, Giros& giros_obj);
     void setup();
     void run();
-    String checkState(uint16_t sensorsPosition[5]);
-    void pararSeOutroStatus(String f_status);
-    void rotinaBasica();
-    String status;
+    uint8_t checkState(uint16_t sensorsPosition[5]);
+    void pararSeOutroStatus(uint8_t f_status);
+    bool segueLinha();
+    bool girar90Graus(char l);
+    bool paralelar();
+    bool alignInPlace();
+    bool gap();
+
+    bool _paralelarLadoE();
+    bool _paralelarLadoD();
+
+    uint8_t status;
   private:
     Lum& _lum;
     Motor& _carro;
+    Giros& _giros;
+    bool _loopBool=true;  // _loopBool é sempre, quando não está sendo usado, _true_. Ou seja, deve sempre existir antes de return false;
+    float _startGirosPos[3];
+    char _paralelarLado;
+    bool _paralelarLadoDone=false;
+    uint8_t _paralelarBest=0;
+    bool _paralelarBestAssigned=false;
 };
 
 #endif
