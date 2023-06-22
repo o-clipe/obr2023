e24fa621b07ec3b153a4e9a816de89c579810a71
O-CLIPE <luksnapo@gmail.com>
2023-06-21 21:06:05 -0300

    Simplifica.

--- lib/Giros/Giros.cpp
+++ lib/Giros/Giros.cpp
@@ -9,7 +9,9 @@ MPU6050 mpu6050(Wire);
 
 Giros::Giros() // Constructor
 {
-    memoriaLastIdx = 0;
+    // #define x 0
+    // #define y 1
+    // #define z 2
 }
 
 
@@ -19,13 +21,6 @@ void Giros::setup() // Chamado no Setup()
     mpu6050.begin();
     mpu6050.calcGyroOffsets(true);
 }
-
-
-void Giros::run() // Comandos de rotina
-{
-    setMemoria();
-}
-
 
 void Giros::print() //
 {
@@ -45,64 +40,4 @@ float* Giros::girosRead()
     _xyz[1] = mpu6050.getAngleY();
     _xyz[2] = mpu6050.getAngleZ();
     return _xyz;
-}
-
-
-void Giros::setMemoria()
-{
-  uint16_t read = millis()%(MEMSIZE*MILLISTEP);
-  uint32_t idx = (read - read%MILLISTEP)/MILLISTEP;
-
-  girosRead();
-  memoria[idx][0] = _xyz[0];
-  memoria[idx][1] = _xyz[1];
-  memoria[idx][2] = _xyz[2];
-
-  memoriaLastIdx = idx;
-}
-
-
-void Giros::defineVariacaoPadrao()
-{
-    float max[3] = {-100, -100, -100};
-    float min[3] = {100, 100, 100}; 
-    for (uint8_t i=0; i < 10; i++)
-    {
-        for (uint8_t j=0; j < 3; j++)
-        {
-            if (memoria[i][j] > max[j])
-            {
-                max[j] = memoria[i][j];
-            }
-            if (memoria[i][j] < min[j])
-            {
-                min[j] = memoria[i][j];
-            }
-        }
-    }
-
-    variacaoPadrao[0] = max[0] - min[0];
-    variacaoPadrao[1] = max[1] - min[1];
-    variacaoPadrao[2] = max[2] - min[2];
-}
-
-bool Giros::detectaVariacao(char eixo)
-{
-    uint8_t anterior = (memoriaLastIdx+MEMSIZE-1)%MEMSIZE;
-    uint8_t e = _traduzCharParaIdx(eixo);
-    if (fabs(memoria[memoriaLastIdx][e] - memoria[anterior][e]) >= variacaoPadrao[e]*FATORERROVARIACAO)
-    {
-        return true;
-    }
-    return false;
-}
-
-uint8_t Giros::_traduzCharParaIdx(char arg)
-{
-    if (arg == 'x') return 0;
-    if (arg == 'y') return 1;
-    if (arg == 'z') return 2;
-    if (arg == 'X') return 0;
-    if (arg == 'Y') return 1;
-    if (arg == 'Z') return 2;
 }
--- lib/Giros/Giros.h
+++ lib/Giros/Giros.h
@@ -7,27 +7,15 @@
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 
-#define MEMSIZE 10
-#define MILLISTEP 500
-#define FATORERROVARIACAO 1.5
-
 class Giros
 {
   public:
     Giros();
     void setup();
-    void run();
     void print();
     float* girosRead();
     void setMemoria();
-    void defineVariacaoPadrao();
-    bool detectaVariacao(char eixo);
-    float memoria[MEMSIZE][3];
-    uint8_t memoriaLastIdx;
-    float variacaoPadrao[3];
-    // MPU6050 mpu6050;
   private:
-    uint8_t _traduzCharParaIdx(char arg);
     float _xyz[3];
     
 };
